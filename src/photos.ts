7d9fb7c452dae66eeb4dff6317a4c10603d56fef
Inna Starovytska <starovitckayainna1@gmail.com>
2024-10-09 21:30:07 +0300

    com-1

---
+++ src/photos.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const API_KEY: string = "cc0b8c9957fa037fbfc04ea8fab5c7d3";
+
+axios.defaults.baseURL = "https://api.unsplash.com/";
+
+interface IParamsUrl {
+  orientation: string;
+  per_page: number;
+  client_id: string;
+}
+
+axios.defaults.params = {
+  orientation: "landscape",
+  per_page: 15,
+  client_id: API_KEY,
+} as IParamsUrl;
+
+export interface Photo {
+  id: string;
+  alt_description: string;
+  urls: {
+    small: string;
+    regular: string;
+  };
+  user: {
+    name: string;
+  };
+  likes: number;
+}
+
+export interface PhotosResponse {
+  results: Photo[];
+  total: number;
+  total_pages: number;
+}
+
+export const getPhotos = async (
+  query: string,
+  page: number
+): Promise<PhotosResponse> => {
+  const { data } = await axios.get<PhotosResponse>(
+    `search/photos?query=${query}&page=${page}`
+  );
+
+  return data;
+};
